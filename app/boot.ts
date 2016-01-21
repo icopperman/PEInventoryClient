44bdb28bda4b5ab11b46dad58e4269c1c87f0941
ira <rozira@optonline.net>
2016-01-20 22:38:58 -0500

    http

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
+//import {Http} from 'angular2/http';
+
 import {Index} from './index';
 
 bootstrap(Index);
--- app/getData.service.ts
+++ app/getData.service.ts
@@ -1,11 +1,43 @@
 import {Injectable} from 'angular2/core';
+import {Http, RequestOptions, RequestOptionsArgs} from 'angular2/http';
 
 @Injectable()
 export class getDataService{
 
+    constructor(public http: Http) {}
+
     getData(url: string, httpfunction: string, data: any) {
 
+        //let ro: RequestOptionsArg = { method: httpfunction};
+
+        switch (httpfunction) {
+
+            case "GET":
+                this.http.get(url)
+                    .map(function(item,idx) {
+                        return item.json();
+                    })
+                    .subscribe(next, error, complete);
+                break;
+            case "POST":
+                this.http.post(url)
+                    .map(function(item,idx) {
+                        return item.json();
+                    })
+                    .subscribe(next, error, complete);
+                break;
+
+        }
         console.log("getdata");
     }
 
+}
+function next(arg) {
+    console.log('next');
+}
+function error(arg) {
+    console.log('error');
+}
+function complete() {
+    console.log('complete');
 }
